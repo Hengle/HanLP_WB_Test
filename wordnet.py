fb60eaa71ff30d631f18933db0e871281a067697
bwangxxx <390297188@qq.com>
2020-01-04 17:29:41 +0800

    02

---
+++ wordnet.py
@@ -0,0 +1,9 @@
+# import nltk
+# nltk.download('wordnet')
+
+from nltk.corpus import wordnet as wn
+
+print(wn.synsets('published'))
+dog = wn.synset('dog.n.01')
+print(dog.hypernyms())
+print(dog.hyponyms())
