fe6be7787a204b8854bd855c01d3d7c0a37c7f68
bwangxxx@Home <390297188@qq.com>
2020-01-13 23:01:13 +0800

    做习题

--- AutomateTheBoringStuffWithPython/addStar.py
+++ AutomateTheBoringStuffWithPython/addStar.py
@@ -7,7 +7,11 @@ text = pyperclip.paste()
 # 分离文本中的行，添加星号。
 lines = text.split('\n')
 
+# 每一行都加个星号
 for i in range(len(lines)):
     lines[i] = '* ' + lines[i]
+
+# 重新连成文本
+text = '\n'.join(lines)
 
 pyperclip.copy(text)
