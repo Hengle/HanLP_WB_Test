d38b9940fa71a97236a1ddf3f2d45a0de38a81c9
bwangxxx@Office <390297188@qq.com>
2020-01-13 14:52:57 +0800

    章内项目练习

--- pw.py
+++ pw.py
@@ -2,16 +2,13 @@
 # coding=gbk
 # pw.py - An insecure password locker program.
 """
-第一行开头的写法"#!"是一种 Unix/Linux 特性，这些操作系统上面的 解释器 指令 允许脚本和数据文件充当系统命令，无需在调用时由用户指定解释器，
-从而对用户和其它程序隐藏其实现细节。
+第一行开头的写法"#!"是一种 Unix/Linux 特性，这些操作系统上面的 解释器 指令 允许脚本和数据文件充当系统命令，
+无需在调用时由用户指定解释器，从而对用户和其它程序隐藏其实现细节。
 假设 /usr/local/bin/foo 中有一以下行开头的 Bourne shell 脚本: #!/bin/sh -x
 而它被如此调用（"$"是命令提示符）: $ foo bar
 这里的 foo 和 bar 类似我们小时候写作文里面的 小明 和 小红，只是随便起的名字，
 该命令的输出等同于: $ /bin/sh -x /usr/local/bin/foo bar
-
-此行在 windows 系统下无意义，windows 通常使用系统环境变量。
-
-git bash 下据说是可以用这一行的。
+注意：此行在 windows 系统下通常无意义，windows 使用系统环境变量来指定 解释器 的位置。
 """
 import sys
 import pyperclip
@@ -26,6 +23,6 @@ if len(sys.argv) < 2:
 account = sys.argv[1]   # 第一个命令行参数是“账户名”
 if account in PASSWORDS:
     pyperclip.copy(PASSWORDS[account])
-    print('Password for ' + account + ' copied to clipboard.')
+    print(account + ' 的密码已经复制到剪贴板，直接按 Ctrl + V 即可粘贴到指定位置。')
 else:
-    print('There is no account named ' + account)
+    print('没有找到 ' + account + ' 账户。')
