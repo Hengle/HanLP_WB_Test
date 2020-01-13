81910ac6c4ab41cb67dea59413a0123eb9d83a9b
bwangxxx@Office <390297188@qq.com>
2020-01-13 14:44:56 +0800

    章内项目练习

--- pw.py
+++ pw.py
@@ -1,4 +1,5 @@
 #! python3
+# pw.py - An insecure password locker program.
 """
 第一行开头的写法"#!"是一种 Unix/Linux 特性，这些操作系统上面的 解释器 指令 允许脚本和数据文件充当系统命令，无需在调用时由用户指定解释器，
 从而对用户和其它程序隐藏其实现细节。
@@ -11,11 +12,19 @@
 
 git bash 下据说是可以用这一行的。
 """
-# pw.py - An insecure password locker program.
-
+import sys
+import pyperclip
 PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
              'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
              'luggage': '12345'}
 
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
 
-
+account = sys.argv[1]   # 第一个命令行参数是“账户名”
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account)
