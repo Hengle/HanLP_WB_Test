627c7535a120a3b3f9c9ed8ac5a2c1789f08386c
bwangxxx@Office <390297188@qq.com>
2020-01-15 15:47:18 +0800

    练习

--- AutomateTheBoringStuffWithPython/phoneAndEmail.py
+++ AutomateTheBoringStuffWithPython/phoneAndEmail.py
@@ -13,6 +13,15 @@ phoneRegex = re.compile(r'''(
     (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
 )''', re.VERBOSE)
 
+phoneRegex2 = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?              # 区号，3个数字，或者括号里的3个数字，或者不存在
+    (\s|-|\.)?                      # 空格，或者-，或者.，或者不存在
+    (\d{3})                         # 前3个数字
+    (\s|-|\.)                       # 空格，或者-，或者.
+    (\d{4})                         # 后4个数字
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?  # (有或无若干空格 + 括号内的ext或者x或者ext. + 有或无若干空格 + 2~5个数字)?
+)''', re.VERBOSE)
+
 # TODO: Create email regex.
 # TODO: Find matches in clipboard text.
 # TODO: Copy results to the clipboard.
