8eba681f88089619b240813950a6e4cfebb99b92
bwangxxx@Home <390297188@qq.com>
2020-01-18 23:28:07 +0800

    装饰器_01

--- Study/Decorator.py
+++ Study/Decorator.py
@@ -1,5 +1,16 @@
+#! python3
+# coding=gbk
+import logging
+
+
 def foo():
     print('i am foo')
+    logging.basicConfig(filename='example.log', level=logging.DEBUG)
+    logging.debug('This message should go to the log file')
+    logging.info('foo is running, So should this')
+    logging.warning('And this, too')
+    logging.error('由于严重的问题，程序的某些功能已经不能正常执行')
+    logging.critical('严重的错误，表明程序已不能继续执行')
 
 
 foo()
