9db6612ccd5d03f31d873ee07623ba5f7e923d53
bwangxxx@Home <390297188@qq.com>
2020-01-19 00:01:27 +0800

    装饰器_01

--- Study/Decorator.py
+++ Study/Decorator.py
@@ -3,13 +3,35 @@
 import logging
 
 
-def use_logging(func):
-    logging.warning("%s is running" % func.__name__)
-    func()
+# 不使用装饰器
+def test_01():
+    def use_logging(func):
+        logging.warning('%s is running' % func.__name__)
+        func()
+
+    def foo():
+        print('i am foo')
+
+    use_logging(foo)
 
 
-def foo():
-    print('i am foo')
+# test_01()
 
 
-use_logging(foo)
+# 简单装饰器
+def test_02():
+    def use_logging(func):
+        def wrapper(*args, **kwargs):
+            logging.warning('%s is running' % func.__name__)
+            # func()
+            return func(*args, **kwargs)
+        return wrapper
+
+    def bar():
+        print('i am bar')
+
+    bar = use_logging(bar)
+    bar()
+
+
+test_02()
