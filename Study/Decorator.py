deedcfa7deb23f09f3d07e6b2377d2ff03c1755b
bwangxxx@Office <390297188@qq.com>
2020-01-19 09:56:13 +0800

    使用语法糖 @modify 代替 b = modify(b)

--- Study/Decorator.py
+++ Study/Decorator.py
@@ -38,16 +38,18 @@ def test_02():
 # test_02()
 
 
-def modify(func):           # 传入 原本需要执行的函数
-    def w():                # 定义 函数 2，函数 2 只是把 修饰器添加的代码 和 原本要执行的函数的代码 打包到一起
+def modify(func):  # 传入 原本需要执行的函数
+    def w():  # 定义 函数 2，函数 2 只是把 修饰器添加的代码 和 原本要执行的函数的代码 打包到一起
         logging.warning('%s is running' % func.__name__)
         return func()
-    return w                # 把打包好的 函数 2 扔回去
+
+    return w  # 把打包好的 函数 2 扔回去
 
 
+@modify
 def b():
-    print('-= x =-')        # 原本需要执行的代码
+    print('-= x =-')  # 原本需要执行的代码
 
 
-x = modify(b)   # 返回的是 修饰器的代码 合并 原本需要执行的代码
-x()
+# x = modify(b)  # 返回的是 修饰器的代码 合并 原本需要执行的代码
+b()
