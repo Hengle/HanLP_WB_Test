03c57bb48ddd6c73f16d16f39e982b38dee7f947
bwangxxx@Home <390297188@qq.com>
2020-01-27 22:07:56 +0800

    scrapy 测试

--- stackoverflow/middlewares.py
+++ stackoverflow/middlewares.py
@@ -101,3 +101,18 @@ class StackoverflowDownloaderMiddleware(object):
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
+
+# mimvp custom by yourself
+class ProxyMiddleware(object):
+    def process_request(self, request, spider):
+
+        if request.url.startswith("http://"):
+            request.meta['proxy'] = "http://127.0.0.1:50420"  # http代理
+        elif request.url.startswith("https://"):
+            request.meta['proxy'] = "http://127.0.0.1:50420"  # https代理
+
+#         # proxy authentication
+#         proxy_user_pass = "USERNAME:PASSWORD"
+#         encoded_user_pass = base64.encodestring(proxy_user_pass)
+#         request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
