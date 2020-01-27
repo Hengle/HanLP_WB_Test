f6347fb2cef9ddd89772262dbf257b99687acb66
bwangxxx@Home <390297188@qq.com>
2020-01-27 14:06:22 +0800

    装饰器_01

---
+++ AutomateTheBoringStuffWithPython/mouseNow.py
@@ -0,0 +1,26 @@
+#! python3
+# coding=gbk
+import pyautogui
+import time
+
+print('Press Ctrl-C to quit.')
+
+while True:
+    x, y = pyautogui.position()
+    positionStr = 'X=' + str(x).rjust(4) + '; Y=' + str(y).rjust(4)
+    print(positionStr, end='')
+    time.sleep(0.1)
+    print('\b' * len(positionStr), end='', flush=True)
+
+"""
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X = ' + str(x).rjust(4) + 'Y = ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+
+except KeyboardInterrupt:
+    print('\nDone.')  # °´ÏÂ Ctrl + C ¼´´òÓ¡ 'Done.'
+
+"""
