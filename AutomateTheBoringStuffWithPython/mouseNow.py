001f3391bfd282bdb4c4ac1d4ccb6878ae8b5c6a
bwangxxx@Home <390297188@qq.com>
2020-01-27 14:10:13 +0800

    装饰器_01

--- AutomateTheBoringStuffWithPython/mouseNow.py
+++ AutomateTheBoringStuffWithPython/mouseNow.py
@@ -9,7 +9,7 @@ while True:
     x, y = pyautogui.position()
     positionStr = 'X=' + str(x).rjust(4) + '; Y=' + str(y).rjust(4)
     print(positionStr, end='')
-    time.sleep(0.1)
+    time.sleep(0.2)
     print('\b' * len(positionStr), end='', flush=True)
 
 """
