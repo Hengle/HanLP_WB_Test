b6b6f576cbf1518dc181693533067e0d1fde3c85
bwangxxx@Home <390297188@qq.com>
2020-01-31 19:08:01 +0800

    scrapy 测试

--- Test/tutorial/tutorial/spiders/quotes_spider.py
+++ Test/tutorial/tutorial/spiders/quotes_spider.py
@@ -6,16 +6,19 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"  # 爬虫的唯一标识
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
 
     # 生成网页链接：可以返回链接的列表，或者写一个链接生成器。
-    def start_requests(self):
-        # urls = [
-        start_urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        # for url in urls:
-        #     yield scrapy.Request(url=url, callback=self.parse)
+    # def start_requests(self):
+    #    urls = [
+    #        'http://quotes.toscrape.com/page/1/',
+    #        'http://quotes.toscrape.com/page/2/',
+    #    ]
+    #    for url in urls:
+    #        yield scrapy.Request(url=url, callback=self.parse)
 
     # parse()方法用来解析相应，它处理为每个请求下载的响应。它提取爬取的数据作为字典，
     # 还查找要遵循的新 URL 并从中创建新请求（Request）。
@@ -24,7 +27,7 @@ class QuotesSpider(scrapy.Spider):
         page = response.url.split("/")[-2]
         logging.warning('response.url.split("/") = %s' % response.url.split("/"))
         # [root] WARNING: response.url.split("/") = ['http:', '', 'quotes.toscrape.com', 'page', '1', '']
-        filename = 'quotes-%s.html' % page
+        filename = 'quotes-wb-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
-        self.log('Saved file %s' % filename)
+        # self.log('Saved file %s' % filename)
