f42d59d7320efc8e681ca578364672af5501bafd
bwangxxx@Home <390297188@qq.com>
2020-01-31 11:41:47 +0800

    scrapy 测试

--- Test/tutorial/tutorial/spiders/quotes_spider.py
+++ Test/tutorial/tutorial/spiders/quotes_spider.py
@@ -5,8 +5,9 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    name = "quotes"  # 爬虫的唯一标识
 
+    # 生成网页链接：可以返回链接的列表，或者写一个链接生成器。
     def start_requests(self):
         urls = [
             'http://quotes.toscrape.com/page/1/',
@@ -15,6 +16,9 @@ class QuotesSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    # parse()方法用来解析相应，它处理为每个请求下载的响应。它提取爬取的数据作为字典，
+    # 还查找要遵循的新 URL 并从中创建新请求（Request）。
+    # response 参数是 TextResponse 的一个实例，它保存着页面内容。
     def parse(self, response):
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
