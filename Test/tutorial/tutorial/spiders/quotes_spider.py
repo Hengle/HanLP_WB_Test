a839e9e8917864af0f71d5b2f7ed9fe8c76ac778
bwangxxx@Home <390297188@qq.com>
2020-02-01 17:44:18 +0800

    scrapy 测试

--- Test/tutorial/tutorial/spiders/quotes_spider.py
+++ Test/tutorial/tutorial/spiders/quotes_spider.py
@@ -6,10 +6,11 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"  # 爬虫的唯一标识
-    start_urls = [
+    start_urls_0 = [  # 开始爬取的资源链接列表
         'http://quotes.toscrape.com/page/1/',
         'http://quotes.toscrape.com/page/2/',
     ]
+    start_urls = ['https://movie.douban.com/chart']
 
     # 生成网页链接：可以返回链接的列表，或者写一个链接生成器。
     # def start_requests(self):
@@ -20,9 +21,9 @@ class QuotesSpider(scrapy.Spider):
     #    for url in urls:
     #        yield scrapy.Request(url=url, callback=self.parse)
 
-    # parse()方法用来解析相应，它处理为每个请求下载的响应。它提取爬取的数据作为字典，
+    # parse()方法用来解析每个 Request 所下载的响应。它提取爬取的数据作为字典，
     # 还查找要遵循的新 URL 并从中创建新请求（Request）。
-    # response 参数是 TextResponse 的一个实例，它保存着页面内容。
+    # response 参数是 TextResponse 的一个实例，它保存着下载下来的页面内容。
     def parse(self, response):
         """
         page = response.url.split("/")[-2]
