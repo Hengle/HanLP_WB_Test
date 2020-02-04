1743818f6bfd83362fd7694b4395dee1985bb4e3
bwangxxx@Home <390297188@qq.com>
2020-02-04 21:28:42 +0800

    scrapy 测试

--- AutomateTheBoringStuffWithPython/test_01.py
+++ AutomateTheBoringStuffWithPython/test_01.py
@@ -1037,3 +1037,54 @@ print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 Robot.h_m()
 Robot.print_class_doc()
 Robot.print_say_hi_doc()
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+
+class SchoolMember:
+    """代表学校里的任何成员。"""
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print('(Initialized SchoolMember: {})'.format(self.name))
+
+    def tell(self):
+        """告诉我细节。"""
+        print('Name:{} Age:{}'.format(self.name, self.age), end=' ')
+
+
+class Teacher(SchoolMember):
+    """代表一位老师。"""
+
+    def __init__(self, name, age, salary):
+        SchoolMember.__init__(self, name, age)
+        self.salary = salary
+        print('(Initialized Teacher: {})'.format(self.name))
+
+    def tell(self):
+        SchoolMember.tell(self)
+        print('Salary:{:d}'.format(self.salary))
+
+
+class Student(SchoolMember):
+    """代表一位学生。"""
+
+    def __init__(self, name, age, marks):
+        SchoolMember.__init__(self, name, age)
+        self.marks = marks
+        print('(Initialized Student: {})'.format(self.name))
+
+    def tell(self):
+        SchoolMember.tell(self)
+        print('Marks:{:d}'.format(self.marks))
+
+
+t = Teacher('WangBo', 42, 30000)
+s = Student('WangZhiYuan', 7, 75)
+
+# 打印一行空白
+print()
+
+members = [t, s]
+for member in members:
+    member.tell()
