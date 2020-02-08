c7929c473b4e6cac9c59d50c6823747c8152718f
bwangxxx@Home <390297188@qq.com>
2020-02-08 15:56:12 +0800

    scrapy 测试

--- AutomateTheBoringStuffWithPython/test_01.py
+++ AutomateTheBoringStuffWithPython/test_01.py
@@ -1222,13 +1222,12 @@ class Fib:
         return fib  # 返回的值从 0 开始
 
 
-print(list(Fib(200)))
-
-fx = Fib(3)
+print('--------------------------------------')
 # for 循环会使用内置的 iter() 函数代替 __iter__()
 # 所以我们可以看到 __iter__() 不会被执行
 # 因此，不要在 __iter__() 中执行 return self 以外的代码
 for idx in range(3):
-    print('斐波那契数列的第 {} 个数是：{}'.format(idx+1, next(fx)))
-
+    print('斐波那契数列的第 {} 个数是：{}'.format(idx + 1, next(fx)))
+print('--------------------------------------')
 print(list(Fib(3)))
+print('--------------------------------------')
