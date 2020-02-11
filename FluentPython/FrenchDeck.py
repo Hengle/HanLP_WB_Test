52e3a64eeb1926f97146e0334ede57ff201d799c
bwangxxx@Home <390297188@qq.com>
2020-02-11 17:28:01 +0800

    scrapy 测试

--- FluentPython/FrenchDeck.py
+++ FluentPython/FrenchDeck.py
@@ -3,9 +3,40 @@
 
 import collections
 
-Card = collections.namedtuple('Card', ['rank', 'suit'])
+Card = collections.namedtuple('Card', 'rank, suit')
+
+print(Card)  # 创建的是一个类
+
+field_names = 'rank, suit'
+if isinstance(field_names, str):
+    field_names = field_names.replace(',', ' ').split()
+    print(field_names)
+    x = map(str, field_names)
+    print(list(x))
 
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'apades diamonds clubs hearts'.split()
+    print(ranks)
+    print(suits)
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+        print(self._cards)
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+A = FrenchDeck()
+print(A.__len__())
+print(A.__getitem__(2))
+print('~~~~~~~~~~~~~~~')
+print(A[0])
+print(A[1])
+print(A[2])
