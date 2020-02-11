f9205bda21b6d36c8d344ebe303f70b266bf5868
bwangxxx@Home <390297188@qq.com>
2020-02-11 12:20:46 +0800

    scrapy 测试

--- FluentPython/FrenchDeck.py
+++ FluentPython/FrenchDeck.py
@@ -1,1 +1,11 @@
+#! python3
+# coding=gbk
+
 import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11) + list('JQKA')]
+    suits = 'apades diamonds clubs hearts'.split()
