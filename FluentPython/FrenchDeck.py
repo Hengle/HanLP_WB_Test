be19123ff61393e5cee74eb79b20bdd939165060
bwangxxx@Home <390297188@qq.com>
2020-02-12 09:25:24 +0800

    scrapy 测试

--- FluentPython/FrenchDeck.py
+++ FluentPython/FrenchDeck.py
@@ -7,26 +7,13 @@ import random
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 
-# print(Card)
-# field_names = 'rank, suit'
-# if isinstance(field_names, str):
-#     field_names = field_names.replace(',', ' ').split()
-#     print(field_names)
-#     x = map(str, field_names)
-#     print(list(x))
-
-
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'apades diamonds clubs hearts'.split()
 
-    # print(ranks)
-    # print(suits)
-
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits
                        for rank in self.ranks]
-        # print(self._cards[0])
 
     def __len__(self):
         return len(self._cards)
@@ -35,10 +22,6 @@ class FrenchDeck:
         return self._cards[position]
 
 
-deck = FrenchDeck()  # 构建一副牌
-print('牌的数量 = %d' % len(deck))
-print('随机抽一张牌：{}'.format(random.choice(deck)))
-
 suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 
 
@@ -46,6 +29,10 @@ def spades_high(card):
     rank_value = FrenchDeck.ranks.index(card.rank)
     return rank_value * len(suit_values) + suit_values[card.suit]
 
+
+deck = FrenchDeck()  # 构建一副牌
+print('牌的数量 = %d' % len(deck))
+print('随机抽一张牌：{}'.format(random.choice(deck)))
 
 # for card in deck:  # doctest: +ELLIPSIS
 #     print(card)
