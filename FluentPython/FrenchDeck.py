53809c4caf5d0dfc233a72b91e1304c3bb0b56bf
bwangxxx@Home <390297188@qq.com>
2020-02-16 21:18:01 +0800

    scrapy 测试

--- FluentPython/FrenchDeck.py
+++ FluentPython/FrenchDeck.py
@@ -148,3 +148,11 @@ invoice = """
 1510  Panavise Jr. - PV-201                $28.00    1    $28.00
 1601  PiTFT Mini Kit 320x240               $34.95    1    $34.95
 """
+SKU = slice(0, 6)
+DESCRIPTION = slice(6, 40)
+UNIT_PRICE = slice(40, 52)
+QUANTITY = slice(52, 55)
+ITEM_TOTAL = slice(55, None)
+line_items = invoice.split('\n')[2:]
+for item in line_items:
+    print(item[DESCRIPTION], item[UNIT_PRICE])
