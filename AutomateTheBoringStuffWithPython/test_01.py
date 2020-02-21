449ac403342938d6bb548eb5de5b502c0c72df69
bwangxxx@Office <390297188@qq.com>
2020-02-21 10:42:57 +0800

    流畅的Python

--- AutomateTheBoringStuffWithPython/test_01.py
+++ AutomateTheBoringStuffWithPython/test_01.py
@@ -789,7 +789,7 @@ print(testpath.split(os.path.sep))  # 本质上是分解字符串，间隔参数是 os.path.sep
 print(os.path.sep)
 
 print('——————————查看文件大小和文件夹内容——————————')
-print(os.path.getsize(os.path.join(download_dir, 'PBSetup70.exe')) / 1)  # 指定文件的大小，字节
+print(os.path.getsize(os.path.join(download_dir, 'mls-mpm88.zip')) / 1)  # 指定文件的大小，字节
 print(os.listdir(download_dir))  # 文件夹中的内容
 print(os.path.getsize(os.path.join(download_dir, 'test')) / 1)  # 指定文件的大小，字节
 
